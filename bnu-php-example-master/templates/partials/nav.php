8719562a60eaeddd4b8bed1cebdc32d010496b69
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-05-03 13:53:57 +0100

    update

--- bnu-php-example-master/templates/partials/nav.php
+++ bnu-php-example-master/templates/partials/nav.php
@@ -2,7 +2,7 @@
 <div id="main-nav">
 
    <!--adding student records to navigation bar-->
-   <a href="student.php">Student Records</a>
+   <a href="student.php">Student Records</a> |
    <a href="modules.php">My Modules</a> |
    <a href="assignmodule.php">Assign Module</a> |
    <a href="details.php">My Details</a> |
