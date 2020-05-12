2a3beef26ce418892195012b51c822c0796e54d3
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-05-12 13:40:01 +0100

    Adding delete button and check boxes

--- bnu-php-example-master/student.php
+++ bnu-php-example-master/student.php
@@ -4,6 +4,25 @@
    include("_includes/dbconnect.inc");
    include("_includes/functions.inc");
 
+   //testing what i am getting through post
+   //echo"<pre>";
+   //print_r($_POST);
+   //echo"</pre>";
+
+   if(!empty($_POST['delete']))
+   {
+     foreach($_POST['delete'] as $student_id => $value)
+     {
+       $sql = "DELETE FROM student"
+
+
+       //sql
+
+       echo "student id  = ". $student_id . "<br/>";
+
+     }
+   }
+
    // check logged in
    if (isset($_SESSION['id']))
    {
@@ -16,20 +35,30 @@
 
    $result = mysqli_query($conn, $sql);
 
+   //wrap table within form which submits when the 'Delete' button is pressed
+   $data['content'] .= "<form method='POST' action=''>";
+
 // prepare the table with the headings
    $data['content'] .= "<table border='1'>";
-   $data['content'] .= "<tr><th colspan='5' align='center'>Student Record</th></tr>";
+   $data['content'] .= "<tr><th colspan='6' align='center'>Student Record</th></tr>";
    $data['content'] .= "<tr><th>Student ID</th><th>DOB</th><th>First Name</th>";
-   $data['content'] .= "<th>Last Name</th><th>Address</th></tr>";
+   $data['content'] .= "<th>Last Name</th><th>Address</th><th>Delete</th></tr>";
 
 // Display student details within the html table
    while($row = mysqli_fetch_array($result))
    {
       $data['content'] .= "<tr><td> $row[studentid] </td><td> $row[dob] </td>";
       $data['content'] .= "<td> $row[firstname] </td><td> $row[lastname] </td>";
-      $data['content'] .= "<td> $row[house] $row[town] $row[county] $row[country] $row[postcode]</td></tr>";
+      $data['content'] .= "<td> $row[house] $row[town] $row[county] $row[country] $row[postcode]</td>";
+      //adding a check box
+      $data['content'] .= "<td><input type='checkbox' name='delete[$row[studentid]]'</td></tr>";
    }
-   $data['content'] .= "</table>";
+   $data['content'] .= "</table>"; //closing html table
+   //adding delete button
+   $data['content'] .= "<input type='submit' value='Delete'/>";
+
+   //closes form
+   $data['content'] .= "</form>";
 
    // render the template
    echo template("templates/default.php", $data);
