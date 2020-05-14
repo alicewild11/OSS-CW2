bf90d374b812b0aca140d35d08d5590ecc29d10a
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-05-14 21:09:32 +0100

    executing sql

--- bnu-php-example-master/addstudent.php
+++ bnu-php-example-master/addstudent.php
@@ -10,6 +10,54 @@
 
       echo template("templates/partials/header.php");
       echo template("templates/partials/nav.php");
+
+      // if the form has been submitted
+      if (isset($_POST['submit'])) {
+
+         // build an sql statment to update the student details
+         $sql = "INSERT INTO student (studentid, firstname, lastname, house, town, county, country postcode)";
+         $sql = $sql . " values ('$_POST[txtstudentid]', '$_POST[txtfirstname]', '$_POST[txtlastname]', '$_POST[txthouse]', 
+         '$_POST[txttown]', '$_POST[txtcounty]', '$_POST[txtcountry]', '$_POST[txtpostcode]', '$_POST[txtpassword]')";
+
+
+         $result = mysqli_query($conn,$sql);
+
+         $data['content'] = "<p>Your details have been inserted</p>";
+
+      }
+      else {
+          // using <<<EOD notation to allow building of a multi-line string
+          $data['content'] = <<<EOD
+
+          <h2>Add Student</h2>
+          <form name="frmaddstudent" action="" method="post">
+          Student ID :
+          <input name="txtstudentid" type="text" /><br/>
+          First Name :
+          <input name="txtfirstname" type="text"  /><br/>
+          Surname :
+          <input name="txtlastname" type="text"   /><br/>
+          Number and Street :
+          <input name="txthouse" type="text" /><br/>
+          Town :
+          <input name="txttown" type="text"   /><br/>
+          County :
+          <input name="txtcounty" type="text" /><br/>
+          Country :
+          <input name="txtcountry" type="text"   /><br/>
+          Postcode :
+          <input name="txtpostcode" type="text" /><br/>
+          Password :
+          <input name="txtpassword" type="text"/><br/>
+          <input type="submit" value="Save" name="submit"/>
+          </form>
+
+          EOD;
+
+      }
+
+      // render the template
+      echo template("templates/default.php", $data);
    }
    else
    {
