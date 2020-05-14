3c504e669e94786d5ccfcb860421893b36da8fe6
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-05-14 16:43:59 +0100

    Add file to nav bar

--- bnu-php-example-master/templates/partials/nav.php
+++ bnu-php-example-master/templates/partials/nav.php
@@ -3,6 +3,7 @@
 
    <!--adding student records to navigation bar-->
    <a href="student.php">Student Records</a> |
+   <a href="addstudent.php">Add Student</a> |
    <a href="modules.php">My Modules</a> |
    <a href="assignmodule.php">Assign Module</a> |
    <a href="details.php">My Details</a> |
