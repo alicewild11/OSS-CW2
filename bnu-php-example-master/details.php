2328375300aa8900abe88a7c2e52999535bfca1b
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-05-20 00:23:58 +0100

    Update

--- bnu-php-example-master/details.php
+++ bnu-php-example-master/details.php
@@ -14,8 +14,15 @@ if (isset($_SESSION['id'])) {
    // if the form has been submitted
    if (isset($_POST['submit'])) {
 
+     //$stmt = $conn->prepare ("UPDATE student set firstname = ? WHERE id = ?");
+     //$stmt->bind_param("si", $_POST['txtfirstname'], $_SESSION['id']);
+     //$stmt->execute();
+
+     //$stmt->close();
+
+
       // build an sql statment to update the student details
-      $sql = "update student set firstname ='" . $_POST['txtfirstname'] . "',";
+      $sql = "UPDATE student set firstname ='" . $_POST['txtfirstname'] . "',";
       $sql .= "lastname ='" . $_POST['txtlastname']  . "',";
       $sql .= "house ='" . $_POST['txthouse']  . "',";
       $sql .= "town ='" . $_POST['txttown']  . "',";
@@ -28,10 +35,13 @@ if (isset($_SESSION['id'])) {
       $data['content'] = "<p>Your details have been updated</p>";
 
    }
-   else {
+   else
+   {
       // Build a SQL statment to return the student record with the id that
       // matches that of the session variable.
-      $sql = "select * from student where studentid='". $_SESSION['id'] . "';";
+
+
+      $sql = "SELECT * from student where studentid='". $_SESSION['id'] . "';";
       $result = mysqli_query($conn,$sql);
       $row = mysqli_fetch_array($result);
 
@@ -39,27 +49,28 @@ if (isset($_SESSION['id'])) {
       // see http://stackoverflow.com/questions/6924193/what-is-the-use-of-eod-in-php for info
       // also http://stackoverflow.com/questions/8280360/formatting-an-array-value-inside-a-heredoc
       $data['content'] = <<<EOD
+       <div>
+       <h2>My Details</h2>
+       <form name="frmdetails" action="" method="post">
+       <p>First Name :<p>
+       <input name="txtfirstname" type="text" value="{$row['firstname']}" /><br/>
+       <p>Surname :<p>
+       <input name="txtlastname" type="text"  value="{$row['lastname']}" /><br/>
+       <p>Number and Street :<p>
+       <input name="txthouse" type="text"  value="{$row['house']}" /><br/>
+       <p>Town :<p>
+       <input name="txttown" type="text"  value="{$row['town']}" /><br/>
+       <p>County :<p>
+       <input name="txtcounty" type="text"  value="{$row['county']}" /><br/>
+       <p>Country :<p>
+       <input name="txtcountry" type="text"  value="{$row['country']}" /><br/>
+       <p>Postcode :<p>
+       <input name="txtpostcode" type="text"  value="{$row['postcode']}" /><br/>
+       <input type="submit" value="Save" name="submit"/>
+       </form>
+       </div>
 
-   <h2>My Details</h2>
-   <form name="frmdetails" action="" method="post">
-   First Name :
-   <input name="txtfirstname" type="text" value="{$row['firstname']}" /><br/>
-   Surname :
-   <input name="txtlastname" type="text"  value="{$row['lastname']}" /><br/>
-   Number and Street :
-   <input name="txthouse" type="text"  value="{$row['house']}" /><br/>
-   Town :
-   <input name="txttown" type="text"  value="{$row['town']}" /><br/>
-   County :
-   <input name="txtcounty" type="text"  value="{$row['county']}" /><br/>
-   Country :
-   <input name="txtcountry" type="text"  value="{$row['country']}" /><br/>
-   Postcode :
-   <input name="txtpostcode" type="text"  value="{$row['postcode']}" /><br/>
-   <input type="submit" value="Save" name="submit"/>
-   </form>
-
-EOD;
+       EOD;
 
    }
 
