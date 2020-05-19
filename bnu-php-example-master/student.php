645b8017a9586f0593a720d6d8e14c09a6f9f802
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-05-19 22:22:18 +0100

    UPDATE

--- bnu-php-example-master/student.php
+++ bnu-php-example-master/student.php
@@ -14,9 +14,12 @@
      foreach($_POST['delete'] as $student_id => $value)
      {
        //delete a record
-       $sql = "DELETE FROM student WHERE studentid = $student_id";
+       $stmt = $conn->prepare("DELETE FROM student WHERE studentid = $student_id");
+       $stmt->bind_param("i",  $student_id);
+       $stmt->execute();
+       $stmt->close();
 
-       $result = mysqli_query($conn, $sql);
+
 
      }
    }
@@ -29,37 +32,37 @@
      echo template("templates/partials/nav.php");
 
 
-   $sql = "SELECT * from student";
+     $sql = "SELECT * from student";
 
-   $result = mysqli_query($conn, $sql);
+     $result = mysqli_query($conn, $sql);
 
-   //wrap table within form which submits when the 'Delete' button is pressed
-   $data['content'] .= "<form method='POST' action=''>";
+     //wrap table within form which submits when the 'Delete' button is pressed
+     $data['content'] .= "<form method='POST' action=''>";
 
-// prepare the table with the headings
-   $data['content'] .= "<table border='1'>";
-   $data['content'] .= "<tr><th colspan='6' align='center'>Student Record</th></tr>";
-   $data['content'] .= "<tr><th>Student ID</th><th>DOB</th><th>First Name</th>";
-   $data['content'] .= "<th>Last Name</th><th>Address</th><th>Delete</th></tr>";
+  // prepare the table with the headings
+     $data['content'] .= "<table border='1'>";
+     $data['content'] .= "<tr><th colspan='6' align='center'>Student Record</th></tr>";
+     $data['content'] .= "<tr><th>Student ID</th><th>DOB</th><th>First Name</th>";
+     $data['content'] .= "<th>Last Name</th><th>Address</th><th>Delete</th></tr>";
 
-// Display student details within the html table
-   while($row = mysqli_fetch_array($result))
-   {
-      $data['content'] .= "<tr><td> $row[studentid] </td><td> $row[dob] </td>";
-      $data['content'] .= "<td> $row[firstname] </td><td> $row[lastname] </td>";
-      $data['content'] .= "<td> $row[house] $row[town] $row[county] $row[country] $row[postcode]</td>";
-      //adding a check box
-      $data['content'] .= "<td><input type='checkbox' name='delete[$row[studentid]]'</td></tr>";
-   }
-   $data['content'] .= "</table>"; //closing html table
-   //adding delete button
-   $data['content'] .= "<input type='submit' value='Delete'/>";
+  // Display student details within the html table
+     while($row = mysqli_fetch_array($result))
+     {
+        $data['content'] .= "<tr><td> $row[studentid] </td><td> $row[dob] </td>";
+        $data['content'] .= "<td> $row[firstname] </td><td> $row[lastname] </td>";
+        $data['content'] .= "<td> $row[house] $row[town] $row[county] $row[country] $row[postcode]</td>";
+        //adding a check box
+        $data['content'] .= "<td><input type='checkbox' name='delete[$row[studentid]]'</td></tr>";
+     }
+     $data['content'] .= "</table>"; //closing html table
+     //adding delete button
+     $data['content'] .= "<input type='submit' value='Delete'/>";
 
-   //closes form
-   $data['content'] .= "</form>";
+     //closes form
+     $data['content'] .= "</form>";
 
-   // render the template
-   echo template("templates/default.php", $data);
+     // render the template
+     echo template("templates/default.php", $data);
 
   }
     else
