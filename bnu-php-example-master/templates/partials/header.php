8e1145fa1aec7352467d56d7b11e0f625ae623a8
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-05-19 22:21:31 +0100

    Including CSS

--- bnu-php-example-master/templates/partials/header.php
+++ bnu-php-example-master/templates/partials/header.php
@@ -3,7 +3,113 @@
    <head>
       <title>BNU Student Web Application</title>
       <!-- styles and JS here -->
+      <style>
 
-   </head>
+      body
+      {
+        background-color: #ffffff
+      }
+      div
+      {
+        padding: 10px;
+      }
+      a
+      {
+          font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+          font-size: 20px;
+          color: #76949F;
+      }
+      a:hover
+      {
+        color: #b3b3b3;
+      }
+      table, th, td
+      {
 
-   <body>
+        font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+        font-size: 15px;
+        border-collapse: collapse;
+        width: 100%;
+        border: 2px solid #000000;
+        padding: 15px;
+      }
+      tr:hover
+      {
+        background-color: #b3b3b3;
+      }
+      th
+      {
+        padding-top: 12px;
+        padding-bottom: 12px;
+        text-align: centre;
+        background-color: #76949F;
+        color: white;
+      }
+      h2
+      {
+        font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+        font-size: 20px;
+        color: #76949F;
+        text-align: center;
+      }
+      p
+      {
+        font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+        color: #76949F;
+      }
+      input[type=text], select
+      {
+        width: 100%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        display: inline-block;
+        border: 1px solid #ccc;
+        border-radius: 90px;
+        box-sizing: border-box;
+      }
+      input[type=date], select
+      {
+        width: 100%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        display: inline-block;
+        border: 1px solid #ccc;
+        border-radius: 90px;
+        box-sizing: border-box;
+      }
+      input[type=password], select
+      {
+        width: 100%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        display: inline-block;
+        border: 1px solid #ccc;
+        border-radius: 90px;
+        box-sizing: border-box;
+
+      }
+
+      input[type=submit]
+      {
+        width: 100%;
+        background-color: #76949F;
+        color: white;
+        padding: 14px 20px;
+        margin: 8px 0;
+        border: none;
+        border-radius: 90px;
+        cursor: pointer;
+      }
+
+      input[type=submit]:hover
+      {
+        background-color: #b3b3b3;
+
+      }
+      div
+      {
+        border-radius: 5px;
+        padding: 2px;
+      }
+</style>
+</head>
